4f6b6f36f9b77a82138d867767dbd1b8393622ef
Alexandre Magno <alexandre.mbm@gmail.com>
2015-06-27 17:20:37 -0300

    Comments on class RepositoryClient

--- paicemana/githubclient.py
+++ paicemana/githubclient.py
@@ -2,12 +2,19 @@ from github3 import GitHub  # TODO in setup.py (package github3.py)
 
 
 class RepositoryClient(object):
+    """Class to read and write on GitHub repository"""
 
     def __init__(self, repo_user = 'OSMBrasil', repo_name = 'semanario'):
+        """
+        @params
+        repo_user - name of organization or user
+        repo_name - repository name
+        """
         github = GitHub()
         self.repo = github.repository(repo_user, repo_name)
 
     def milestones_info(self):
+        """Prints information for identification of milestones"""
         s = ''
         for milestone in self.repo.milestones():
             s += 'The milestone "%s" has number=%s\n'\
