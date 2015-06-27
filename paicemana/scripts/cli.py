02a0146584e8287cac3c493ae7ffd27d330594d5
Alexandre Magno <alexandre.mbm@gmail.com>
2015-06-27 14:40:52 -0300

    Fixes #1

--- paicemana/scripts/cli.py
+++ paicemana/scripts/cli.py
@@ -4,16 +4,21 @@ import datetime
 import click
 
 from paicemana.textdownload import MarkdownDownload
+from urllib.error import HTTPError
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-g', '--get', type=int,
+@click.option('-g', '--archive', type=int,
     help='Number in permalink like www.weeklyosm.eu/archives/4205')
 def cli(archive):
     """A helper script for works at OSMBrasil/semanario"""
-    MarkdownDownload(archive)
-
+    if not archive:
+        raise click.UsageError('try the -h/--help option')
+    try:
+        MarkdownDownload(archive)
+    except HTTPError as e:
+        click.echo(e)
 
