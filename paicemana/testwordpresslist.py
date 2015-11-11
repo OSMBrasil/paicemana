57af755c61eb931524bffef7df423086d3aa4c39
Alexandre Magno <alexandre.mbm@gmail.com>
2015-11-11 06:30:21 -0300

    paicemana/testwordpresslist.py for WEEKLYOSM.htm

--- paicemana/testwordpresslist.py
+++ paicemana/testwordpresslist.py
@@ -1,54 +1,56 @@
-"""
-10/27/2015-11/2/2015
+from lxml import html
+import re
 
-[caption id="attachment_12684" align="aligncenter" width="807"]<a href="http://blog.openstreetmap.de/wp-uploads//2015/11/before-after-road-styles-2.jpg"><img class=" wp-image-12684" src="http://blog.openstreetmap.de/wp-uploads//2015/11/before-after-road-styles-2.jpg" alt="neuer Carto Map Style auf osm.org" width="807" height="606" /></a> New OSM Carto Map Style at osm.org <a href="#wn276_maps">[1]</a> Data: OpenStreetMap-Contributors, Image CC-BY-SA 2.0 <a href="http://www.openstreetmap.org/">OpenStreetMap.org</a>[/caption]
-<h2 id="wn276_mapping">Mapping</h2>
-<ul>
-...
-http://blog.openstreetmap.de/wp-uploads//2015/11/before-after-road-styles-2.jpg
-http://blog.openstreetmap.de/wp-uploads//2015/11/before-after-road-styles-2.jpg
-"""
+def get_wordpress_ids_for_weeks():
 
-from wordpress_xmlrpc import Client
-from wordpress_xmlrpc.methods import posts
+    # save http://www.weeklyosm.eu/wp-admin/edit.php as "HTML only"
 
+    tree = html.parse('WEEKLYOSM.htm')
 
-def test_pages(user, password):
-    client = Client('http://www.weeklyosm.eu/xmlrpc.php', user, password)
-    results = client.call(posts.GetPosts())
-    p = results[0]
-    print(p.id)
-    #print(p.title)
+    anchors = tree.xpath('//a[@class="row-title"]')
+
+    # prices = tree.xpath('//span[@class="item-price"]/text()')
+
+    wordpress_weeks = {}
+
+    for a in anchors:
+        text = a.text
+        link = a.get('href')
+        if re.match(r'[Ww]eekly +[\d-]+', text) and re.match(r'\S+post=\d+&action=edit', link):
+            week_number = text.split()[1]
+            tmp = re.sub(r'\S+post=', '', link)
+            tmp = re.sub(r'&action=edit', '', tmp)
+            wordpress_id = int(tmp)
+            #print(week_number, " ", wordpress_id)
+            multiple = week_number.split('-')
+            tmp = []
+            for i in multiple:
+                tmp.append(int(i))
+            multiple = list(tmp)
+            print(multiple, wordpress_id)
+
+            for i in multiple:
+                wordpress_weeks[i] = {'wordpress':wordpress_id}
+                if len(multiple) > 1:
+                    tmp = list(multiple)
+                    tmp.remove(i)
+                    wordpress_weeks[i]['multiple'] = tmp
+
+    return wordpress_weeks
 
 
 if __name__ == "__main__":
-    test_pages('alexandre', 'SENHA')
+    print(get_wordpress_ids_for_weeks())
 
-# Getting all posts is broken
 
-"""
-alexandre@notebook-itautec paicemana $ python paicemana/testwordpresslist.py
-Traceback (most recent call last):
-  File "paicemana/testwordpresslist.py", line 56, in <module>
-    test_pages('alexandre', '*******')
-  File "paicemana/testwordpresslist.py", line 49, in test_pages
-    results = client.call(posts.GetPosts())
-  File "/usr/lib/python3.5/site-packages/python_wordpress_xmlrpc-2.3-py3.5.egg/wordpress_xmlrpc/base.py", line 37, in call
-  File "/usr/lib/python3.5/xmlrpc/client.py", line 1091, in __call__
-    return self.__send(self.__name, args)
-  File "/usr/lib/python3.5/xmlrpc/client.py", line 1431, in __request
-    verbose=self.__verbose
-  File "/usr/lib/python3.5/xmlrpc/client.py", line 1133, in request
-    return self.single_request(host, handler, request_body, verbose)
-  File "/usr/lib/python3.5/xmlrpc/client.py", line 1149, in single_request
-    return self.parse_response(resp)
-  File "paicemana/testwordpresslist.py", line 39, in parse_response
-    p.feed(data)
-  File "/usr/lib/python3.5/xmlrpc/client.py", line 438, in feed
-    self._parser.Parse(data, 0)
-xml.parsers.expat.ExpatError: not well-formed (invalid token): line 1010, column 196
+""" Exemplo (quebrado) com dados fictícios:
+{
+    "277": {
+        "osmbc": 36335,
+        "wordpress": 262625
+        "multiple": [18181, 17171, 17171],
+        "image": "5751"
+    }
+}
 """
 
-# http://www.scriptscoop.com/t/59768a9a3cf4/xml-rpc-python-client-raising-xml-parsers-expat-expaterror-exception.html
-
-# http://www.sourcecodebrowser.com/python3.2/3.2.3~rc1/classxmlrpc_1_1client_1_1_transport.html
